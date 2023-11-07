b801f0ff39e2b6a7bb5294e1d56feb67da0d4c43
Matheus Paizante <matheus_paizante@hotmail.com>
2023-11-06 22:12:12 -0300

    .

--- Semana-03/AAAAAAA/pilha.h
+++ Semana-03/AAAAAAA/pilha.h
@@ -1,9 +1,28 @@
 
 
 typedef int TipoItem;
+const int max_itens = 100;
+
 
 class pilha{
     private:
+    int tamanho;
+    TipoItem* estrutura;
+
+
 
     public:
+    pilha();//construtora
+    ~pilha();//destrutora
+    bool estaCheia();//verifica se a pilha esta cheia
+    bool estaVazia();//verifica se a pilha esta vazia
+    void inserir(TipoItem item);//push
+    TipoItem remover();//pop
+    void imprimir();//print
+    int qualTamanho();//lenght
+
+
+
+
+
 };
