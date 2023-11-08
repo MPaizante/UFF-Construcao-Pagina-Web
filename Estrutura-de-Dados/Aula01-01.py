7be41cb4cd0e0b00fa542ef996d70dce2565cfd6
Matheus Paizante <matheus_paizante@hotmail.com>
2023-11-07 22:28:34 -0300

    Update Aula01-01.py

--- Estrutura-de-Dados/Aula01-01.py
+++ Estrutura-de-Dados/Aula01-01.py
@@ -3,7 +3,12 @@ def perm(data, n):
         print(data)
         return
     for i in range(n):
-        prem(data, n - 1)
+        perm(data, n - 1)
         if n % 2 == 0:
-            data[i], data[n-1]
+            data[i], data[n-1] = data[n-1],data[i]
+        else:
+            data[0],data[n-1] = data[n-1],data[0]
+data =[1,2,3]
+perm(data,len(data))
+            
         
