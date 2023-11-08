acf58aa1b33539719dc2c14c202d5c10d5714912
Matheus Paizante <matheus_paizante@hotmail.com>
2023-11-08 19:38:28 -0300

    Update Aula02-04.py

--- Estrutura-de-Dados/Aula02-04.py
+++ Estrutura-de-Dados/Aula02-04.py
@@ -8,3 +8,7 @@ else:
     print("Tudo certo")
 finally:
     print("Fim do tratamento")
+###############################################################
+num =-1
+if num <1:
+    raise Exception("Erro! Valor não permitido")
