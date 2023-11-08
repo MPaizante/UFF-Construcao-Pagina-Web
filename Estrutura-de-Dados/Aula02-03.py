600bf1c92e9f61d518e5a0b02ca02251a11fabd3
Matheus Paizante <matheus_paizante@hotmail.com>
2023-11-08 17:23:46 -0300

    Update Aula02-03.py

--- Estrutura-de-Dados/Aula02-03.py
+++ Estrutura-de-Dados/Aula02-03.py
@@ -13,14 +13,27 @@ class Carro:
         estado= "Sim" if c1.ligado else "Não"
         print("O carro está ligado: ",estado)
         print("---------------------------------------")
-        
+    def ligar(self):
+        self.ligado=True
+    def desligar(self):
+        self.ligado=False      
+    def andar(self):
+        if self.ligado:
+            print("Andando!")
+        else:
+            print("Parado!")  
         
 
 c1=Carro(200,False,"Balck")
 c2=Carro(120,False,"White")
 c3=Carro(350,False,"Red")
 
+c1.ligar()
+c2.ligar()
+
 c1.mostrar()
 c2.mostrar()
 c3.mostrar()
 
+c1.andar()
+c2.andar()
